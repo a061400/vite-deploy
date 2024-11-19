7b0e6eab38dbb3165736607a4c9d51c6d4e258d5
shawnlin <a061400@gmail.com>
2024-11-19 14:09:50 +0800

    first

---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    這是一個卡片元件
+    <br />
+    <br />
+    <br />
+    <br />
+    {{ text }}
+    <br />
+    <br />
+    <br />
+    <br />
+    {{data}}
+    <br />
+    <br />
+    <br />
+    <br />
+    {{envir}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "一段文字",
+      data: {},
+      envir:"",
+    };
+  },
+
+  mounted() {
+    const url = import.meta.env.VITE_PATH;
+    this.envir = import.meta.env.VITE_TEXT;
+    this.$http.get(url).then((res) => {
+        this.data = res.data.results[0];
+    })
+  },
+};
+</script>
